590928ff94a07886744347ddba3e511c76c4791b
joao-dotcom <r6joao@gmail.com>
2024-05-03 17:48:12 -0300

    .

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
+  
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex,nofollow">
     <meta NAME="robots" CONTENT="noarchive">
@@ -18,9 +18,7 @@
 
 <body>
     <div class="main-wrapper">
-        <!-- -------------------------------------------------------------- -->
-        <!-- Preloader - style you can find in spinners.css -->
-        <!-- -------------------------------------------------------------- -->
+       
         <div class="preloader">
         <svg class="tea lds-ripple" width="37" height="48" viewbox="0 0 37 48" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M27.0819 17H3.02508C1.91076 17 1.01376 17.9059 1.0485 19.0197C1.15761 22.5177 1.49703 29.7374 2.5 34C4.07125 40.6778 7.18553 44.8868 8.44856 46.3845C8.79051 46.79 9.29799 47 9.82843 47H20.0218C20.639 47 21.2193 46.7159 21.5659 46.2052C22.6765 44.5687 25.2312 40.4282 27.5 34C28.9757 29.8188 29.084 22.4043 29.0441 18.9156C29.0319 17.8436 28.1539 17 27.0819 17Z" stroke="#2962FF" stroke-width="2"></path>
@@ -30,12 +28,7 @@
           <path id="steamR" d="M21 6C21 6 21 8.22727 19 9.5C17 10.7727 17 13 17 13" stroke="#2962FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
         </svg>
     </div>
-        <!-- -------------------------------------------------------------- -->
-        <!-- Preloader - style you can find in spinners.css -->
-        <!-- -------------------------------------------------------------- -->
-        <!-- -------------------------------------------------------------- -->
-        <!-- Login box.scss -->
-        <!-- -------------------------------------------------------------- -->
+    
         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style='background-image: url("assets/imagens/fundo_novo.webp");background-repeat: repeat-x;'>
             <div class="auth-box">
                 <div id="loginform">
@@ -49,7 +42,7 @@
                             @endforeach
                         @endif
                     </div>
-                    <!-- Form -->
+                
                     <div class="row">
                         <div class="col-12">
                             <form class="form-horizontal mt-3" method="GET" action="{{ route('home.index') }}">
@@ -79,21 +72,14 @@
                 
             </div>
         </div>
-        <!-- -------------------------------------------------------------- -->
-        <!-- Login box.scss -->
-        <!-- -------------------------------------------------------------- -->
+   
     </div>
-    <!-- -------------------------------------------------------------- -->
-    <!-- All Required js -->
-    <!-- -------------------------------------------------------------- -->
+
     <script src="{{ url ('painel/assets/libs/jquery/dist/jquery.min.js') }}"></script>
    
-    <!-- Bootstrap tether Core JavaScript -->
+
     <script src="{{ url ('painel/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- -------------------------------------------------------------- -->
-    <!-- This page plugin js -->
-    <!-- -------------------------------------------------------------- -->
-    <!--Custom JavaScript -->
+ 
     <script src="{{ url ('painel/dist/js/feather.min.js') }}"></script>
     <script src="{{ url ('painel/dist/js/custom.min.js') }}"></script>
 
